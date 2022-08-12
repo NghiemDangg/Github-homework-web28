ece0d1bec4fd5aae88883e45c079cf2196d0a7b8
Nghiêm Đăng <nghiemkhachaidang25@gmail.com>
2022-08-12 17:08:07 +0700

    add day 12

---
+++ day12/script.js
@@ -0,0 +1,122 @@
+/**
+ * Kiểm tra số một số nguyên dương bất kỳ có phải số nguyên tố hay không
+ *
+ * Số nguyên tố là số chỉ chia hết cho `1` và chính nó
+ *
+ * @param {number} n Số nguyên dương bất kỳ
+ *
+ * @return {boolean} Số `n` cho có phải số nguyên tố hay không
+ */
+function isPrime(n) {
+  var flag = true;
+  if (n < 2) {
+    flag = false; //n < 2 không phải số nguyên tố
+  } else {
+    for (let i = 2; i < n - 1; i++) {
+      if (n % i == 0) {
+        flag = false;
+        break;
+      }
+    }
+  }
+  if (flag == true) {
+    return flag;
+  } else {
+    return (flag = false);
+  }
+}
+
+// console.log(isPrime(2));
+// console.log(isPrime(3));
+// console.log(isPrime(90));
+// console.log(isPrime(7));
+
+/**
+ * Tính tổng các số nguyên tố trong khoảng từ 1 đến n
+ *
+ * @param {number} n Số nguyên dương bất kỳ
+ *
+ * @return {number} Tổng các số nguyên tố trong khoảng từ `1` đến `n`
+ */
+function sumOfPrimes(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+// console.log(sumOfPrimes(9));
+/**
+ * Tìm số Fibonacci thứ n
+ *
+ * Dãy Fibonacci là dãy số bắt đầu bằng 0 và 1, các số tiếp theo bằng tổng 2 số trước nó
+ *
+ * VD: 0 1 1 2 3 5 8 13 ... (Số thứ 1 là 0, số thứ 2 là 1, số thứ 3 là 2, ...)
+ *
+ * VD: `n = 5`, kết quả số Fibonacci thứ 5 là `3`
+ *
+ * @param {number} n Số nguyên dương bất kỳ (n > 0)
+ *
+ * @return {number} Số Fibonacci thứ `n`
+ */
+function fibonacci(n) {
+  let n1 = 0;
+  n2 = 1;
+  let nextTerm;
+  for (let i = 1; i <= n; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
+}
+// console.log(fibonacci(5));
+/**
+ * Tính giai thừa của một số nguyên bất kỳ
+ *
+ * Giai thừa của một số `n = 1 * 2 * 3 * ... * (n - 1) * n`
+ *
+ * @param {number} n Số nguyên dương bất kỳ (n >= 0)
+ *
+ * @return {number} Giai thừa của `n`
+ */
+function factorial(n) {
+  var result = 1;
+  if (n > 1) {
+    for (var i = 1; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+}
+// console.log(factorial(9));
+// console.log(factorial(0));
+/**
+ * Kiểm tra một số nguyên dương bất kỳ có phải số Strong hay không
+ *
+ * Số Strong là số có tổng giai thừa các chữ số bằng chính nó
+ *
+ * VD: 145 = 1! + 4! + 5! = 1 + 24 + 120 = 145
+ *
+ * @param {number} n Số nguyên dương bất kỳ
+ *
+ * @return {boolean} Số `n` có phải là số Strong hay không
+ */
+function isStrong(n) {
+  var sum = 0;
+  var originNumber = n;
+  while (n > 0) {
+    var lastDigit = n % 10;
+    var fact = factorial(lastDigit);
+    sum = sum + fact;
+    n = n /10;
+  }
+  if (sum == originNumber) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isStrong(145));
