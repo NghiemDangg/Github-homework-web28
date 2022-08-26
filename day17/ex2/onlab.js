1ecf4e1627113964770224f21212649763f78f5b
Nghiêm Đăng <nghiemkhachaidang25@gmail.com>
2022-08-26 18:07:17 +0700

    edit ex2

--- day17/ex2/onlab.js
+++ day17/ex2/onlab.js
@@ -8,7 +8,7 @@ nodeTitle.textContent = heading;
 let nodeHeading = document.querySelector(".heading");
 nodeHeading.innerHTML = heading;
 // 3. Thay đổi src của thẻ img thành giá trị của avatar
-let nodeImg = document.querySelector("img");
+let nodeImg = document.querySelector(".avatar");
 nodeImg.src = avatar;
 // 4. Thay đổi src của thẻ link thành giá trị của profile
 let nodeLink = document.querySelector(".profile");
@@ -23,7 +23,7 @@ nodeUl[lastIndex].textContent = "Sleep";
 // ngày ngày/tháng/năm
 let now = new Date();
 let currentTime = now.toLocaleString("vi-VN");
-console.log(currentTime);
+
 let nodeDiv = document.querySelector(".clock");
 let createNode = document.createElement("p");
 createNode.innerHTML = currentTime;
@@ -31,9 +31,13 @@ nodeDiv.appendChild(createNode);
 // 8. Đếm số từ trong đoạn văn p.bio xem có bao nhiêu từ Ba (không phân biệt chữ hoa chữ
 // thường) và thêm nội dung vào thẻ p.num-of-words bên dưới thông báo kết quả dạng Ở đoạn
 // văn trên có xx từ 'Ba'
-
 let nodePara = document.querySelector(".bio").textContent.toLowerCase();
 let count = nodePara.split("ba").length - 1;
 document.querySelector(".num-of-words").textContent = count;
 // 9. Thêm một hàng mới vào table (tham khảo API của HTMLTableElement)
+let table = document.querySelector(".list-students");
+table.style.border = "1px solid black";
+let row = table.insertRow(0);
 // 10. Thêm một cột (ô) mới vào hàng vừa tạo có nội dung Thầy giáo Ba siêu cấp đẹp trai
+let cell = row.insertCell(0);
+cell.innerHTML = "Thầy giáo Ba siêu cấp đẹp trai";
