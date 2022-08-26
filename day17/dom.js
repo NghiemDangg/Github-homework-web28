43d0a9dbd9e058e23b54637e7356b9f6d011cb72
Nghiêm Đăng <nghiemkhachaidang25@gmail.com>
2022-08-26 15:13:17 +0700

    add ex1

---
+++ day17/dom.js
@@ -0,0 +1,30 @@
+// Truy cập vào thẻ h1 có id=“heading” thay đổi màu chữ thành ‘red’, và in hoa nội dung của thẻ đó
+let changeHeading = document.querySelector("#heading");
+changeHeading.style.color = "red";
+changeHeading.style.textTransform = "uppercase";
+// Thay đổi màu chữ của tất cả thẻ có class “para” thành màu “blue” và có font-size = “20px”
+let changePara = document.querySelectorAll(".para");
+for (let i = 0; i < changePara.length; i++) {
+  changePara[i].style.color = "blue";
+}
+// Thêm thẻ a link đến trang ‘facebook’ ở đằng sau thẻ có class “para-3”
+  let para3 = document.querySelector(".para-3");
+  let createLinkTag = document.createElement("a");
+  createLinkTag.href = "https://vi-vn.facebook.com/";
+  createLinkTag.innerText = "facebook.com";
+  para3.appendChild(createLinkTag);
+
+// Thay đổi nội dung của thẻ có id=“title” thành “Đây là thẻ tiêu đề”
+let changeTitle = document.querySelector("#title");
+changeTitle.innerHTML = "Đây là thẻ tiêu đề";
+// Thêm class “text-bold” vào thẻ có class=“description” (định nghĩa class “text-bold” có tác dụng in đậm chữ) (Không bắt buộc, nếu làm được thì làm)
+// Thay thế thẻ có class=“para-3” thành thẻ button có nội dung là “Click me”
+let changePara3 = document.querySelector(".para-3");
+// changePara3.innerHTML = '<button> click me </button>';
+// Copy thẻ có class=“para-2” và hiển thị ngay đằng sau thẻ đó
+let addPara2 = document.querySelector(".para-2");
+let clone = addPara2.cloneNode(true);
+addPara2.appendChild(clone);
+// Xóa thẻ có class=“para-1”
+let removePara1 = document.querySelector(".para-1");
+// removePara1.remove();
