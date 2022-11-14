6fe40a4b5776e55afcfcf171a4f8925052e56ed3
Nghiêm Đăng <nghiemkhachaidang25@gmail.com>
2022-11-14 20:58:39 +0700

    lab1-doned

--- reactJS/day1/app.js
+++ reactJS/day1/app.js
@@ -1,10 +1,22 @@
 // Kiểm tra chuỗi đỐi xứng
 
-function checkReversed(str){
-    let string1 = "";
-    string1 = str.toLowerCase().split("").reverse("").join("");
-    let string2 = str.toLowerCase();
-    return string1 == string2;
+function checkReversed(str) {
+  let string1 = "";
+  string1 = str.toLowerCase().split("").reverse("").join("");
+  let string2 = str.toLowerCase();
+  return string1 == string2;
 }
-console.log(checkReversed("tvt"));
-console.log(checkReversed("tvt2"));
+
+function checkReversed2(str) {
+  if (str.length <= 1) {
+    return true;
+  }
+  for (let i = 0; i <= Math.floor(str.length / 2); i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(checkReversed2("tvt"));
+console.log(checkReversed2("tvt2"));
