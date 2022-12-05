b3f26916ec3b14475a0bfc93a2a32cfa1971d919
Nghiêm Đăng <nghiemkhachaidang25@gmail.com>
2022-12-05 18:44:55 +0700

    add code Search bar

--- reactJS/useEffect1/my-app/src/Search.js
+++ reactJS/useEffect1/my-app/src/Search.js
@@ -1,11 +1,36 @@
 import React,{ useState, useEffect} from "react";
-
 export default function SearchBar(){
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.json())
+        .then((posts) => {
+          setPosts(posts);
+        });
+    }, [posts]);
+
+    const [searchInput, setSearchInput] = useState('');
+
+    useEffect(()=>{
+        const handleChage = (e)=>{
+            e.prevenDefault();
+            setSearchInput(e.target.value);
+        };
+
+        if(searchInput.length>0){
+            posts.filter((post) =>{
+                return post.title.match(searchInput);
+            })
+        }
+
+        // document.getElementById("#search-box").addEventListener("onchange",handleChage())
+    },[])
+
     return(
         <div>
             <input 
-            className="search-box"
-            placeholder="search"
+            id="search-box"
+            value={searchInput}
             />
         </div>
         )
